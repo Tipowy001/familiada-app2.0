9922b1f342d0245d703175c25fc7e8a854f1a3c2
Tipowy001 <teipesolo@gmail.com>
2025-04-29 22:07:35 +0200

    Poprawka routingu Expressa dla Reacta (fix Cannot GET /)

--- server/index.js
+++ server/index.js
@@ -1,56 +1,35 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server, {
-  cors: {
-    origin: "*",
-  }
+const io = socketIO(server);
+
+const PORT = process.env.PORT || 10000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 io.on('connection', (socket) => {
   console.log('✅ Nowe połączenie');
 
-  socket.on('send_question', (data) => {
-    socket.broadcast.emit('receive_question', data);
-  });
-
-  socket.on('show_answer', (data) => {
-    socket.broadcast.emit('show_answer', data);
-  });
-
-  socket.on('wrong_answer', (data) => {
-    io.emit('wrong_answer', data);
-  });
-
-  socket.on('update_scores', (data) => {
-    io.emit('update_scores', data);
-  });
-
-  socket.on('current_team', (data) => {
-    io.emit('current_team', data);
-  });
-
-  socket.on('reveal_all', () => {
-    io.emit('reveal_all');
-  });
-
-  socket.on('switch_team', (data) => {
-    io.emit('switch_team', data);
-  });
-
-  socket.on('team_names', (data) => {
-    io.emit('team_names', data);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('❌ Rozłączenie');
-  });
+  socket.on('send_question', (data) => socket.broadcast.emit('receive_question', data));
+  socket.on('show_answer', (data) => socket.broadcast.emit('show_answer', data));
+  socket.on('wrong_answer', (data) => io.emit('wrong_answer', data));
+  socket.on('update_scores', (data) => io.emit('update_scores', data));
+  socket.on('current_team', (data) => io.emit('current_team', data));
+  socket.on('reveal_all', () => io.emit('reveal_all'));
+  socket.on('switch_team', (data) => io.emit('switch_team', data));
+  socket.on('team_names', (data) => io.emit('team_names', data));
+  socket.on('round_end', () => io.emit('round_end'));
+  socket.on('disconnect', () => console.log('❌ Rozłączenie'));
 });
 
-const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`🚀 Serwer działa na porcie ${PORT}`);
 });
